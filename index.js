dd916e5828c7cb7599441fbf4b2cdea4d041fbff
ljfwmgedu <admin@ljfwmg.edu.kg>
2025-11-28 10:46:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http
 const UUID = process.env.UUID || 'ce91479b-8eb5-4366-a75c-29f5286262fd'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nezha.389001.xyz:8008';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
-const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const NEZHA_KEY = process.env.NEZHA_KEY || '6ogI7Hfqle9f8OlSyciPdwRmT47VwmN3';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'koyeb1.ljlook.qzz.io';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiZGJjNDI0MGM5YzdkNzIxODg1MjI3YzEwYzkxYjFkZDgiLCJ0IjoiYmYzZTFhNjUtZTdhZS00YThlLWJhZTEtZDk0MjVkNTMwZWU5IiwicyI6IllUUmtOakJoTkRRdE5tWmlOaTAwTnpZMUxXSmxNamd0WkdSalptTXdaamhqT1dGbSJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+const NAME = process.env.NAME || 'koyeb1';                        // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
